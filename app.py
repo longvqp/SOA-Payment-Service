eb0656e84363ee30f359ed4e8311d14f969ba61b
Long Vũ <vuquangphilong@gmail.com>
2022-10-06 11:19:09 +0700

    Inital Commit

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import os
 
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
+bootstrap = Bootstrap5(app)
 
 @app.route('/', methods=['GET','SET'])
 def index():
