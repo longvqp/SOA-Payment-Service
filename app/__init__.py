dd2a682f97486825ebb4dcb0ca40c5fe0feea3f7
Long Vũ <vuquangphilong@gmail.com>
2022-10-22 16:36:08 +0700

    Issue with Flask_Login

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,12 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from config import config
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_login import LoginManager
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -15,7 +17,8 @@ def create_app(config_name):
     config[config_name].init_app(app)
     bootstrap.init_app(app)
     db.init_app(app)
-
+    login_manager.init_app(app)
+    
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
