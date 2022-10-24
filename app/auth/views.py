ebbe3ff0647b6fd5305c2ba8c8923d634a5aaaa9
Long Vũ <vuquangphilong@gmail.com>
2022-10-24 11:45:11 +0700

    Finished Basic Account Register/Login

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,10 @@
-from flask import render_template, flash
+from flask import render_template, redirect, request, url_for, flash
+from flask_login import login_user, logout_user, login_required, current_user
 from . import auth
-from .forms import LoginForm
+from .forms import LoginForm, RegisterForm
 from ..models import User
+from .. import db
+
 
 @auth.route('/login', methods=['GET','POST'])
 def login():
@@ -10,6 +13,7 @@ def login():
         user = User.query.filter_by(masv=form.masv.data.lower()).first()
         
         print(form.masv.data)
+        print(user)
 
 
         if user is not None and user.verify_password(form.password.data):
@@ -18,5 +22,26 @@ def login():
             if next is None or not next.startswith('/'):
                 next = url_for('main.index')
             return redirect(next)
-        flash('Invalid email or password.')
+        flash('Invalid user or password.')
     return render_template('auth/login.html',form=form)
+
+@auth.route('/register', methods=['GET','POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(masv=form.masv.data,
+                    email=form.email.data.lower(),
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        print(user)
+        flash('User Registered')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
+
+@auth.route('/logout')
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('main.index'))
