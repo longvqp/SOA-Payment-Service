2c83982059ed07f472091319e61b34e67e6de7cf
Long Vũ <vuquangphilong@gmail.com>
2022-10-26 19:59:38 +0700

    Finished Update Balance

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,8 @@
 from flask import render_template
 from . import main
-from .forms import retrieve_info
+from .forms import retrieve_info,UpdateBallanceForm
 from flask_login import current_user
+from .. import db
 
 @main.route('/')
 def index():
@@ -12,6 +13,13 @@ def tuition():
     form = retrieve_info()
     return render_template('tuition.html',form=form)
 
-@main.route('/info')
+@main.route('/info', methods=['GET','POST'])
 def info():
-    return render_template('info.html')
+    update_ballance = UpdateBallanceForm()
+    if update_ballance.validate_on_submit():
+        print(update_ballance.amount_of_monney.data)
+        print(current_user.sodu)
+        current_user.sodu = update_ballance.amount_of_monney.data
+        # user = User(sodu=update_ballance.amount_of_monney.data)
+        db.session.commit()
+    return render_template('info.html',update_ballance=update_ballance)
